4819b9283f567e99ea35cdf63b95d35b453bce22
PatrickSachs <kontakt@patrick-sachs.de>
2020-07-04 15:18:29 +0200

    Pot files

---
+++ command/pot.js
@@ -0,0 +1,53 @@
+const { parseVueFiles } = require('../vue-files');
+const path = require('path');
+const fs = require('fs');
+
+function potCommand(command) {
+  const { vueFiles, output } = command;
+
+  const resolvedVueFiles = path.resolve(process.cwd(), vueFiles);
+  const parsedVueFiles = parseVueFiles(resolvedVueFiles);
+
+  let text = '';
+
+  for(let i = 0; i < parsedVueFiles.length; i++) {
+    const res = parsedVueFiles[i];
+    // Meta data
+    text += '#: ' + removeInitialSlash(res.file) + ':' + res.line + '\n';
+    text += 'msgid ' + JSON.stringify(res.path) + '\n';
+    text += 'msgstr ""\n';
+    text += '\n';
+  }
+
+  console.log(text);
+
+  if (output) {
+    const outPath = path.resolve(process.cwd(), output);
+    writeToFile(text, outPath);
+    console.log(`The pot file has been has been saved to ${outPath}`);
+  }
+}
+
+function removeInitialSlash(str) {
+  if (str.startsWith('/')) {
+    return str.substring(1, str.length);
+  }
+}
+
+async function writeToFile(reportString, writePath) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(
+      writePath,
+      reportString,
+      (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      },
+    );
+  });
+}
+
+module.exports = potCommand;
--- index.js
+++ index.js
@@ -2,19 +2,35 @@
  'use strict';
  const program = require('commander');
  const listCommand = require('./command/list');
+ const potCommand = require('./command/pot');
  
+// List
  program
-   .command('list', { isDefault: true })
-   .description('Lists all language strings used in your Vue files.')
-   .requiredOption(
-     '-v, --vueFiles <vueFiles>',
-     'The Vue.js file(s) you want to extract i18n strings from. It can be a path to a folder or to a file. It accepts glob patterns. (ex. *, ?, (pattern|pattern|pattern)',
-   )
-   .option(
-     '-o, --output <output>',
-     'Use if you want to create a json file out of your list. (ex. -o output.json)',
-   )
-   .action(listCommand);
+  .command('list')
+  .description('Lists all language strings used in your Vue files.')
+  .requiredOption(
+    '-v, --vueFiles <vueFiles>',
+    'The Vue.js file(s) you want to extract i18n strings from. It can be a path to a folder or to a file. It accepts glob patterns. (ex. *, ?, (pattern|pattern|pattern)',
+  )
+  .option(
+    '-o, --output <output>',
+    'Use if you want to create a json file out of your list. (ex. -o output.json)',
+  )
+  .action(listCommand)
+
+// Pot
+program
+  .command('pot')
+  .description('Creates a .pot file for translators.')
+  .requiredOption(
+    '-v, --vueFiles <vueFiles>',
+    'The Vue.js file(s) you want to extract i18n strings from. It can be a path to a folder or to a file. It accepts glob patterns. (ex. *, ?, (pattern|pattern|pattern)',
+  )
+  .option(
+    '-o, --output <output>',
+    'Use if you want to create a .pot file out of your list. (ex. -o output.pot)',
+  )
+  .action(potCommand);
  
  program.parseAsync(process.argv);
  
